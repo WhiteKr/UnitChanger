a2e756cb34aa9ac82a679318c826c9bbcd8af2f9
WhiteKr <hanjisu2215g@gmail.com>
2020-11-17 10:51:25 +0900

    Upload UnitChanger

---
+++ unitChanger.js
@@ -0,0 +1,13 @@
+class UnitChangerAPI {
+	ByteCal(bytes) {
+		var bytes = parseInt(bytes);
+		var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "ZB", "HB"];
+
+		var e = Math.floor(Math.log(bytes) / Math.log(1024));
+
+		if (e == "-Infinity") return "0 " + s[0];
+		else return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
+	}
+}
+
+module.exports = new UnitChangerAPI();
