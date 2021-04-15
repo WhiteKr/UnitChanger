7c3ad9933164adbaab7c349b34a0b759bc5ad79a
WhiteKr <hanjisu2215g@gmail.com>
2021-04-15 15:05:36 +0900

    Fixed merge miss

--- unitChanger.js
+++ unitChanger.js
@@ -1,17 +1,9 @@
 class UnitChangerAPI {
-<<<<<<< HEAD
     ByteCal(bytes) {
         var bytes = parseInt(bytes);
-        var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "ZB", "HB"];
+        var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
         var e = Math.floor(Math.log(bytes) / Math.log(1024));
-=======
-	ByteCal(bytes) {
-		var bytes = parseInt(bytes);
-		var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-		
-		var e = Math.floor(Math.log(bytes) / Math.log(1024));
->>>>>>> 06e81d12aeb15e56fce89e6b0402d6d20a0d226d
 
         if (e == "-Infinity") return "0 " + s[0];
         else return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
