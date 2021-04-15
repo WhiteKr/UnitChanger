7238fe9541df728fcde6d067583f8e571832ad00
WhiteKr <hanjisu2215g@gmail.com>
2021-04-15 15:01:22 +0900

    Auto format removed empty endline :p

--- unitChanger.js
+++ unitChanger.js
@@ -1,13 +1,13 @@
 class UnitChangerAPI {
-	ByteCal(bytes) {
-		var bytes = parseInt(bytes);
-		var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "ZB", "HB"];
+    ByteCal(bytes) {
+        var bytes = parseInt(bytes);
+        var s = ["bytes", "KB", "MB", "GB", "TB", "PB", "ZB", "HB"];
 
-		var e = Math.floor(Math.log(bytes) / Math.log(1024));
+        var e = Math.floor(Math.log(bytes) / Math.log(1024));
 
-		if (e == "-Infinity") return "0 " + s[0];
-		else return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
-	}
+        if (e == "-Infinity") return "0 " + s[0];
+        else return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
+    }
 }
 
 module.exports = new UnitChangerAPI();
